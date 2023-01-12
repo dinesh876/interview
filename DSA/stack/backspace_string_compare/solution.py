f977565bad50833646c542de9d41621af804034a
dinesh876 <dineshadventure15@gmail.com>
2023-01-12 17:24:06 +0530

    added backspace string compare code

---
+++ DSA/stack/backspace_string_compare/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        def solve(s):
+            stack = []
+            for c in s:
+                if c != "#":
+                    stack.append(c)
+                elif stack:
+                    stack.pop()
+            return ''.join(stack)
+        return solve(s) ==  solve(t)
